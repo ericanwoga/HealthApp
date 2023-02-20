6805c9a275d7439e37653df45c33067c10d790df
Jay Desmarais <jdesmara@terpmail.umd.edu>
2023-02-19 20:33:29 -0500

    Tab 4

--- code.js
+++ code.js
@@ -6,7 +6,7 @@ function main() {
     //make list empty
 }
 
-function addEntry(){
+function addEntry() {
     //get the value 
     let entry = document.getElementById("entry").value;
     list.push(entry);
@@ -25,7 +25,7 @@ function addEntry(){
 
 }
 
-function completeEntry(b){
+function completeEntry(b) {
     //get the specific entry-item
     let item = b.parentNode.parentNode;
     let key = item.id;
@@ -35,9 +35,27 @@ function completeEntry(b){
     document.getElementById(key).style.textDecoration = "line-through";
 }
 
-function deleteEntry(b){
+function deleteEntry(b) {
     //get the specific entry-item
     let item = b.parentNode.parentNode;
     let key = item.id;
     document.getElementById(key).style.display = "none";
+}
+
+function profileClick() {
+    document.getElementById("profileWarning").style.display = "block"
+}
+
+function displayText() {
+    const radioButtons = document.getElementsByName("radio");
+    let selectedRadioButton = "";
+    for (let i = 0; i < radioButtons.length; i++) {
+        if (radioButtons[i].checked) {
+            selectedRadioButton = radioButtons[i].value;
+            break;
+        }
+    }
+    const dropdownValue = document.getElementById("dropdown").value;
+    const displayText = `You selected "${selectedRadioButton}" and "${dropdownValue}"`;
+    document.getElementById("display").innerHTML = displayText;
 }
