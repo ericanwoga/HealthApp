4f63044671a6681c6258c7fdee7c0b990fe091a5
NeelJ2 <70960187+NeelJ2@users.noreply.github.com>
2023-03-27 21:04:56 -0400

    User can input their weight and it's displayed
    Still need to store the user weight, and have that be store in an array and graph those weights

--- src/content/pages/bodyPage/Body.js
+++ src/content/pages/bodyPage/Body.js
@@ -1,12 +1,24 @@
-import React from 'react';
-import { Header } from 'semantic-ui-react'
-  
+import React, { useState } from "react";
+import { Header } from "semantic-ui-react";
+
 const Body = () => {
+  const [weight, setWeight] = useState("");
+
+  const weightChange = (event) => {
+    setWeight(event.target.value);
+  };
+
   return (
     <div>
-        <Header>Body</Header>
+      <Header>
+        Good Evening,
+        <br />
+        ___!
+      </Header>
+      <p>Today's weight is: {weight} lbs</p>
+      <input type="number" value={weight} onChange={weightChange} />
     </div>
   );
 };
-  
+
 export default Body;
