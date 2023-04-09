b225896b8eef51eea42b7dd85c42e33f51506045
Jay Desmarais <jdesmara@terpmail.umd.edu>
2023-04-09 14:59:04 -0400

    Fixing linter errors

--- src/content/pages/bodyPage/Body.js
+++ src/content/pages/bodyPage/Body.js
@@ -1,65 +1,64 @@
-import React, { useState } from "react";
-import { Header, Input, Button, Grid } from "semantic-ui-react";
-import PageItem from "../../PageItem";
-import { BarChart, Bar, XAxis, YAxis, CartesianGrid, ResponsiveContainer } from 'recharts';
+import React, { useState } from 'react'
+import { Header, Input, Button, Grid } from 'semantic-ui-react'
+import PageItem from '../../PageItem'
 
 const BodyContent = () => {
-  const [weight, setWeight] = useState("");
-  const [submittedWeight, setSubmittedWeight] = useState("");
-  let data = [];
+    const [weight, setWeight] = useState('')
+    const [submittedWeight, setSubmittedWeight] = useState('')
+    let data = []
 
-  const WeightChange = (e) => {
-    setWeight(e.target.value);
-  };
+    const WeightChange = (e) => {
+        setWeight(e.target.value)
+    }
 
-  const SubmitWeight = () => {
-    const parsedWeight = parseInt(weight);
-    setSubmittedWeight(parsedWeight);
-    data.push({ submittedWeight: parsedWeight });
-  };
+    const SubmitWeight = () => {
+        const parsedWeight = parseInt(weight)
+        setSubmittedWeight(parsedWeight)
+        data.push({ submittedWeight: parsedWeight })
+    }
 
-  const ClearData = () => {
-    data = [];
-  };
+    const ClearData = () => {
+        data = []
+    }
 
-  return (
-    <>
-      <Header size="small">
-        {submittedWeight
-          ? `You weighed in at ${submittedWeight} lbs. today!`
-          : "Please enter today's weight"}
-      </Header>
-      <Grid>
-        <Grid.Column width={10} columns={3} divided>
-          <Input
-            fluid
-            label={{ basic: true, content: "lbs" }}
-            labelPosition="right"
-            placeholder="Enter weight..."
-            onChange={WeightChange}
-          />
-        </Grid.Column>
-        <Grid.Column width={6}>
-          <Button fluid onClick={SubmitWeight}>
+    return (
+        <>
+            <Header size="small">
+                {submittedWeight
+                    ? `You weighed in at ${submittedWeight} lbs. today!`
+                    : "Please enter today's weight"}
+            </Header>
+            <Grid>
+                <Grid.Column width={10} columns={3} divided>
+                    <Input
+                        fluid
+                        label={{ basic: true, content: 'lbs' }}
+                        labelPosition="right"
+                        placeholder="Enter weight..."
+                        onChange={WeightChange}
+                    />
+                </Grid.Column>
+                <Grid.Column width={6}>
+                    <Button fluid onClick={SubmitWeight}>
             Submit
-          </Button>
-          <Button fluid onClick={ClearData}>
+                    </Button>
+                    <Button fluid onClick={ClearData}>
             Clear
-          </Button>
-        </Grid.Column>
-      </Grid>
-    </>
-  );
-};
+                    </Button>
+                </Grid.Column>
+            </Grid>
+        </>
+    )
+}
 
 const Body = () => {
-  return (
-    <div>
-      <Header>Body</Header>
-      <PageItem title="Today's Weight" content={<BodyContent />} />
-    </div>
-    
-  );
-};
+    return (
+        <div>
+            <Header>Body</Header>
+            <PageItem title="Today's Weight" content={<BodyContent />} />
+        </div>
 
-export default Body;
+    )
+}
+
+export default Body
