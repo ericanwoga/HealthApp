163b0c305e05c97d93479a55f9f8e0a67ddfeb17
NeelJ2 <70960187+NeelJ2@users.noreply.github.com>
2023-04-10 13:57:06 -0400

    user input clears upon submission
    buttons look better

--- src/content/pages/bodyPage/Body.js
+++ src/content/pages/bodyPage/Body.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { Header, Input, Button, Grid } from 'semantic-ui-react'
+import { Header, Input, Button, Grid, Dropdown } from 'semantic-ui-react'
 import PageItem from '../../PageItem'
 import { BarChart, Bar, XAxis, YAxis, CartesianGrid, ResponsiveContainer } from 'recharts'
 
@@ -13,10 +13,11 @@ const BodyContent = ({ data, setData }) => {
     const handleSubmitWeight = () => {
         const parsedWeight = parseInt(weight)
         setData([...data, { weight: parsedWeight }])
+        setWeight('')
     }
 
     const handleClearData = () => {
-        setData([])
+        setData(data.slice(0, -1))
     }
 
     return (
@@ -27,19 +28,22 @@ const BodyContent = ({ data, setData }) => {
                     : "Please enter today's weight"}
             </Header>
             <Grid>
-                <Grid.Column width={10} columns={3} divided>
+                <Grid.Column width={16} columns={2} divided>
                     <Input
                         fluid
                         label={{ basic: true, content: 'lbs' }}
                         labelPosition="right"
                         placeholder="Enter weight..."
                         onChange={handleWeightChange}
+                        value={weight}
                     />
                 </Grid.Column>
-                <Grid.Column width={6}>
+                <Grid.Column width={8}>
                     <Button fluid onClick={handleSubmitWeight}>
             Submit
                     </Button>
+                </Grid.Column>
+                <Grid.Column width={8}>
                     <Button fluid onClick={handleClearData}>
             Clear
                     </Button>
@@ -60,7 +64,19 @@ const GraphContent = ({ data }) => {
         </ResponsiveContainer>
     )
 }
-
+const options = [
+    { key: 1, text: 'Choice 1', value: 1 },
+    { key: 2, text: 'Choice 2', value: 2 },
+    { key: 3, text: 'Choice 3', value: 3 }
+]
+const DropdownExampleSelection = () => (
+    <Dropdown
+        placeholder='Choose your goal!'
+        fluid
+        selection
+        options={options}
+    />
+)
 const Body = () => {
     const [data, setData] = useState([])
 
@@ -69,6 +85,7 @@ const Body = () => {
             <Header>Body</Header>
             <PageItem title="Today's Weight" content={<BodyContent data={data} setData={setData} />} />
             <PageItem title="Weights" content={<GraphContent data={data} />} />
+            <PageItem title="Calories" content={<DropdownExampleSelection />} />
         </div>
     )
 }
