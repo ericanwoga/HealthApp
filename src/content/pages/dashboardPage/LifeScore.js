27edbfa65907d1736ce613058ce9d88e92a1780c
Jay Desmarais <jdesmara@terpmail.umd.edu>
2023-04-12 19:31:19 -0400

    Fixing life score

--- src/content/pages/dashboardPage/LifeScore.js
+++ src/content/pages/dashboardPage/LifeScore.js
@@ -1,12 +1,34 @@
-import React from 'react'
-import { Progress, Button } from 'semantic-ui-react'
+import React, { useState } from 'react'
+import { Progress, Button, Header } from 'semantic-ui-react'
 import PageItem from '../../PageItem'
+import PageModal from '../../PageModal'
+
+const ModalContent = () => {
+    return (
+        <>
+            <Header>Sorry!</Header>
+            <Header>We are still working on our proprietary way of calculating your Life Score.</Header>
+            <Header>For now, your life score will always be 100!</Header>
+            <Header>Keep up the great work!</Header>
+        </>
+    )
+}
 
 const LifeScoreContent = ({ score }) => {
+    const [lifeScoreModal, setLifeScoreModal] = useState(false)
+
     return (
         <div>
+            <PageModal
+                title="How to improve your life score"
+                content={<ModalContent/>}
+                open={lifeScoreModal}
+                setOpen={() => setLifeScoreModal(true)}
+                setClosed={() => setLifeScoreModal(false)}
+                submitAction={() => setLifeScoreModal(false)}
+                submitText={'Done'}/>
             <Progress value={score} total='100' progress='ratio' size='large' style={{ marginBottom: '2rem' }}/>
-            <Button size='large' fluid>See how to improve this score!</Button>
+            <Button size='large' fluid onClick={() => setLifeScoreModal(!lifeScoreModal)}>See how to improve this score!</Button>
         </div>
     )
 }
