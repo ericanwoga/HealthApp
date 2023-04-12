b3c4cf96232bd8e4d6a7e1768de9a71c73569e06
Jay Desmarais <jdesmara@terpmail.umd.edu>
2023-04-12 02:30:15 -0400

    Creating visual for tracked workouts as well as setting up pagination for use in "view all" section of activity, meals, and more

--- src/content/pages/activityPage/RecentActivity.js
+++ src/content/pages/activityPage/RecentActivity.js
@@ -1,9 +1,69 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PageItem from '../../PageItem'
+import { Header, Card, Pagination } from 'semantic-ui-react'
+import moment from 'moment'
 
-const RecentActivity = () => {
+const Paginate = ({ itemList, limit, itemsPerPage }) => {
+    const [activePage, setActivePage] = useState(1)
+    itemsPerPage = itemsPerPage > 0 ? itemsPerPage : 10
+
     return (
-        <PageItem title="Recent Activity" moreLabel="View More" content={<></>}/>
+        <>
+            {limit
+                ? itemList.slice((activePage - 1) * itemsPerPage, limit)
+                : (<>
+                    {itemList.slice((activePage - 1) * itemsPerPage, activePage * itemsPerPage)}
+                    {(itemList.length >= itemsPerPage)
+                        ? <Pagination
+                            onPageChange={(e, { activePage }) => setActivePage(activePage)}
+                            boundaryRange={0}
+                            defaultActivePage={1}
+                            ellipsisItem={null}
+                            firstItem={null}
+                            lastItem={null}
+                            siblingRange={1}
+                            totalPages={Math.ceil(itemList.length / itemsPerPage)}
+                        />
+                        : ''} </>
+                )
+            }
+        </>
+    )
+}
+
+const WorkoutCard = ({ date, workout }) => {
+    return (
+        <Card as="h5" fluid>
+            <Card.Content>
+                <Card.Header textAlign='left'>{workout.name}</Card.Header>
+                <Card.Meta textAlign='left'>{moment(date).calendar()}</Card.Meta>
+                <Card.Description textAlign='left'>Calories Burned: {workout.calories}</Card.Description>
+            </Card.Content>
+        </Card>
+    )
+}
+
+const WorkoutList = ({ workouts }) => {
+    const workoutsArray = []
+    Object.keys(workouts).map((key) => (
+        workoutsArray.push(
+            <WorkoutCard key={key} date={key} workout={workouts[key]}></WorkoutCard>
+        )
+    ))
+
+    return (
+        <>
+            {workoutsArray.length > 0
+                ? <Paginate itemList={workoutsArray} limit={3}/>
+                : <Header>{'Track your first workout and you\'ll see it here!'}</Header>
+            }
+        </>
+    )
+}
+
+const RecentActivity = ({ userData }) => {
+    return (
+        <PageItem title="Recent Activity" moreLabel="View More" content={<WorkoutList workouts={userData.activityData.activity} limit={3}/>}/>
     )
 }
 
